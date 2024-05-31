62118d411f93c55ff1724d4de6b546b32061c387
heydarov93 <heyderov.yashar@mail.ru>
2024-05-31 21:04:43 +0400

    methods added to remove, list, find tasks

--- simple_task_management/models/task_manager.py
+++ simple_task_management/models/task_manager.py
@@ -3,25 +3,37 @@ from typing import Self, Any
 
 class TaskManager:
     def __init__(self: Self):
-        self._tasks: list[Task] = []
+        self._tasks: dict[str, Task] = {}
     
     @property
-    def tasks(self: Self) -> list[Task]:
+    def tasks(self: Self) -> dict[str, Task]:
         return self._tasks
     
     @tasks.setter
     def tasks(self: Self, value: Any) -> None:
-        if type(self._tasks) == list:
+        if type(self._tasks) == dict:
             raise AttributeError("You can't directly assign value to " +
                                  "`tasks`, use `add_task` method to create " +
                                  "new task")
         
     def add_task(self: Self, title: str, description: str) -> int:
         new_task: Task = Task(title, description)
-        self._tasks.append(new_task)
+        self._tasks[str(new_task.id)] = new_task
         return new_task.id
     
-    def show_tasks(self: Self) -> list[Task]:
-        return self.tasks
-        
+    def remove_task(self: Self, id: int) -> dict[str, Task]:
+        self.tasks[id].mark_as_completed()
+    
+    def list_tasks(self: Self) -> None:
+        for task in self.tasks.values():
+            print(f'id: {task.id}')
+            print(f'title: {task.title}')
+            print(f'status: {'Completed' if task.completed else 'Not completed'}')
+            print('===============\n')
 
+    def find_task(self: Self, id: int) -> Task:
+        try:
+            task = self.tasks[str(id)]
+            return task
+        except KeyError:
+            return None      
