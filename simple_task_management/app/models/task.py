74e0d0142d98e0b7a573284c29bd6ecce1e4fbfa
heydarov93 <heyderov.yashar@mail.ru>
2024-06-02 20:53:16 +0400

    file moved into new directory and remove method modified

---
+++ simple_task_management/app/models/task.py
@@ -0,0 +1,39 @@
+from typing import Self, Union
+
+class Task:
+    id: int = 0
+
+    def __init__(self: Self, title, description):
+        Task.id: int = Task.id + 1
+        self.id: int = Task.id
+        self.title: str = title
+        self.description: str = description
+        self.completed: bool = False
+
+    def mark_as_completed(self):
+        self.completed = True
+    
+    @property
+    def title(self: Self) -> str:
+        return self._title
+    
+    @title.setter
+    def title(self: Self, value: str):
+        if (value == ''):
+            raise ValueError('Please enter at least 1 (one) character for title')
+
+        if (type(value) is not str):
+            raise TypeError('Title must be a string')
+
+        self._title: str = value
+    
+    @property
+    def description(self: Self) -> str:
+        return self._description
+    
+    @description.setter
+    def description(self: Self, value: str):
+        if (type(value) is not str):
+            raise TypeError('Description must be a string')
+
+        self._description: str = value
